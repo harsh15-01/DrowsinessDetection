92689872977f8ce514f8243747ee473b0db9b725
Rishab Kumar <xcalliber62@gmail.com>
2023-02-26 17:02:30 +0530

    basic files

--- main.py
+++ main.py
@@ -69,5 +69,24 @@ while True:
         rightEyeHull = cv2.convesHull(rightEye)
         cv2.drawContours(frame, [leftEyeHull], -1, (0,255,0), 1)
         cv2.drawContours(frame, [rightEyeHull], -1,(0,255,0), 1)
-        
+        if ear < EYE_AR_THRESH:
+            COUNTER+=1
+            if COUNTER >= EYE_AR_CONSEC_FRAMES:
+                if not ALARM_ON:
+                    ALARM_ON = True
+                    if args["alarm"] != "":
+                        t = Thread(target=sound_alarm, args=(args["alarm"],))
+                        t.deamon = True
+                        t.start()
+                cv2.putText(frame, "DROWSINESS ALERT!", (10,30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
+        else:
+            COUNTER = 0
+            ALARM_ON = False
+            cv2.putText(frame, "EAR: {:.2f}".format(ear), (300,30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+    if key == ord("q"):
+        break
+cv2.destroyAllWindows()
+vs.stop()
 
