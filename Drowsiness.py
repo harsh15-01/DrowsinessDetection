e72bf54ced89af125f7b20e8eb79b46e4181eb67
Atig <2005090@kiit.ac.in>
2023-04-29 18:25:09 +0530

    Adding facial recognition

--- Drowsiness.py
+++ Drowsiness.py
@@ -2,34 +2,99 @@ from scipy.spatial import distance as dist
 from imutils.video import VideoStream
 from imutils import face_utils
 from threading import Thread 
-from keras import models
 import numpy as np
-import playsound
+# from playsound import playsound
+from pydub import AudioSegment
+from pydub.playback import play
 import argparse
 import imutils
 import time
-# import dlib 
+import dlib 
 import cv2
 
-model = models.load_model('C:\\Users\\Spyder\\Dev\\DrowsinessDetection\\ManualModel\\drowniness_stage1.h5')
-                            
-img_size = 256
-cap = cv2.VideoCapture(0)
+# def sound_alarm(path):
+#     playsound.playsound(path)
 
-while cap.isOpened():
+song = AudioSegment.from_wav('music.wav')
 
-    ret, frame = cap.read()
-    # backtorgb = cv2.cvtColor(frame, cv2.COLOR_GRAY2RGB)
-    # new_array = cv2.resize(backtorgb, (img_size, img_size))
-    frame = np.array(frame).reshape(-1,img_size, img_size, 1)
-    # Make detections 
-    results = model(frame)
-    
-    # cv2.imshow('Arnav Randi', np.squeeze(results.render()))
-    cv2.imshow('Arnav Randi', results)
-    # image = cv2.resize(image, (480, 640))
-    
-    if cv2.waitKey(10) & 0xFF == ord('q'):
-        break
-cap.release()
-cv2.destroyAllWindows()
+
+def eye_aspect_ratio(eye):
+    A = dist.euclidean(eye[1], eye[5])
+    B = dist.euclidean(eye[2], eye[4])
+
+    C = dist.euclidean(eye[0], eye[3])
+
+    ear = (A+B)/(2.0*C)
+
+    return ear
+
+def Drowsy():
+    ap = argparse.ArgumentParser()
+# ap.add_argument("-p","--shape-predictor", required=True, help="path to facial landmark predictor")
+# ap.add_argument("-a","--alarm", type=str, default="", help="path alarm .WAV file")
+#ap.add_argument("-w","--webcam", type=int, default=0, help="index of webcam on system")
+    args = vars(ap.parse_args())
+
+    EYE_AR_THRESH = 0.3
+    EYE_AR_CONSEC_FRAMES = 48
+
+    COUNTER=0
+    ALARM_ON = False
+
+    print("[info] loading facial landmark predictor...")
+    detector = dlib.get_frontal_face_detector()
+    # predictor = dlib.shape_predictor(args["shape_predictor"])
+    predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
+
+    (lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+    (rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+    print("[info] starting video stream thread.....")
+    vs = VideoStream(1).start() #change to argse when using external webcam
+    time.sleep(1.0)
+
+    while True:
+        frame = vs.read()
+        frame = imutils.resize(frame, width=450)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        rects = detector(gray,0)
+
+        for rect in rects:
+
+            shape = predictor(gray, rect)
+            shape = face_utils.shape_to_np(shape)
+
+            leftEye = shape[lStart:lEnd]
+            rightEye = shape[rStart:rEnd]
+            leftEAR = eye_aspect_ratio(leftEye)
+            rightEAR = eye_aspect_ratio(rightEye)
+
+            ear = (leftEAR+rightEAR)/2.0
+
+            leftEyeHull = cv2.convexHull(leftEye)
+            rightEyeHull = cv2.convexHull(rightEye)
+            cv2.drawContours(frame, [leftEyeHull], -1, (0,255,0), 1)
+            cv2.drawContours(frame, [rightEyeHull], -1,(0,255,0), 1)
+            if ear < EYE_AR_THRESH:
+                COUNTER+=1
+                if COUNTER >= EYE_AR_CONSEC_FRAMES:
+                    if not ALARM_ON:
+                        ALARM_ON = True
+                        # if args["alarm"] != "":
+                        #     t = Thread(target=sound_alarm, args=(args["alarm"],))
+                        #     t.deamon = True
+                        #     t.start()
+                        play(song)
+                    cv2.putText(frame, "DROWSINESS ALERT!", (10,30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
+            else:
+                COUNTER = 0
+                ALARM_ON = False
+                cv2.putText(frame, "EAR: {:.2f}".format(ear), (300,30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+        if key == ord("q"):
+            break
+    cv2.destroyAllWindows()
+    vs.stop()
+
